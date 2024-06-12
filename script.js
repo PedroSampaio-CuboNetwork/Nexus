4c95e694b170d45a91a93b6ff751c74927021f71
Pedro <pedro.sampaio@cubo.network>
2024-06-12 08:49:09 -0300

    Finalizandp

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-const userInput = document.querySelector("#botaoPesquisar");
+const inputPesquisa = document.querySelector("#inputPesquisa");
 const lugarResultado = document.querySelector("#ResultadoDaPesquisa");
+const poke = ["Pichau", "Bulba", "Charchar", "Turgue"];
 
-function pesquisar(){
-    userInput.innerHTML = "";
-    let pesquisa = userInput.value;
-    lugarResultado = document.createElement("nav");
-    lugarResultado.innerHTML = `
-    <h2>O resultados para "${pesquisa}" são:</h2>
-    <p>Pichau</p>`;
+function pesquisar() {
+    lugarResultado.innerHTML = '';
+    let pesquisa = inputPesquisa.value.toLowerCase(); 
+    let encontrado = false; 
 
+    for (const pokemon of poke) {
+        if (pesquisa === pokemon.toLowerCase()) {
+            let resultado = document.createElement("div");
+            resultado.innerHTML = `
+                <h2>O resultado para "${pesquisa}" é:</h2>
+                <p>${pesquisa} - Poke inicial Indigo</p>
+            `;
+            lugarResultado.appendChild(resultado);
+            encontrado = true; 
+            break; 
+        }
+    }
+
+    if (!encontrado) {
+        let resultado = document.createElement("div");
+        resultado.innerHTML = `
+            <h2>Nenhum resultado encontrado para "${pesquisa}".</h2>
+        `;
+        lugarResultado.appendChild(resultado);
+    }
 }
