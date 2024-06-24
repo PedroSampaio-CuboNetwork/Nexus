a71cee219a83e5fbee601fe676fe0e776caf0927
Pedro Sampaio <pedrosampaio@MacBook-Pro-de-Pedro.local>
2024-06-24 11:38:11 -0300

    Finalizando o projeto, Front e Back estão conectados

--- back/src/app.py
+++ back/src/app.py
@@ -1,23 +1,21 @@
 import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from ia import cuboIa
 
 app = Flask(__name__)
+CORS(app)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.route('/ask', methods=['POST', 'GET'])
+@app.route("/ask", methods=["POST"])
 def ask():
     data = request.get_json()
-    pergunta = data['pergunta']
+    pergunta = data["pergunta"]
     resposta = cuboIa(pergunta)
-    data = {
-        'resposta': resposta
-    }
-    json_string = json.dumps(data)
-    return json_string
+    return jsonify({"resposta": resposta})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- front/script.js
+++ front/script.js
@@ -1,32 +1,32 @@
 let perguntaInput = document.querySelector("#inputPesquisa");
-
+console.log
 function pesquisar() {
     let pergunta = perguntaInput.value;
-    fetch('/ask', {
-        method: 'POST',
+    fetch("http://127.0.0.1:5000/ask", {
+        method: "POST",
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
             pergunta: pergunta
-        })
+        }),
     })
     .then(response => {
         if (!response.ok) {
-            throw new Error('Erro na requisição');
+            throw new Error("Erro na requisição");
         }
         return response.json();
     })
     .then(data => {
         if (data && data.resposta) {
             console.log(data.resposta)
-            document.getElementById('ResultadoDaPesquisa').innerHTML = data.resposta;
+            document.getElementById("ResultadoDaPesquisa").innerHTML = data.resposta;
         }
         else {
-            throw new Error('Resposta inválida do servidor');
+            throw new Error("Resposta inválida do servidor");
         }
     })
     .catch(error => {
-        console.error('Erro ao pesquisar a resposta', error);
+        console.error("Erro ao pesquisar a resposta", error);
     });
 }
